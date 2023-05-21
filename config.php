68eec998e5085dcf9b1ecec4e0aef319d0d58b68
Jose Linux <jperezp24@miumg.edu.gt>
2023-05-21 09:02:26 -0600

    Ajuste file config

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
   
   $db = [
-    'host' => 'localhost',
-    'username' => 'userprovoto',
+    'host' => 'localhost', //Nombre de la base de datos
+    'username' => 'userprovoto', //Usuairo de la base de datos
     'password' => '$Abc123**',   //contraseña
     'db' => 'DB_PRO_TSE'       //Nombre de tu base de datos
 ];
