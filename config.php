f74f773e73d535b9bd4011f851f77c55f829921d
Jose Linux <jperezp24@miumg.edu.gt>
2023-05-21 08:52:36 -0600

    commit inicial

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+  
+  $db = [
+    'host' => 'localhost',
+    'username' => 'userprovoto',
+    'password' => '$Abc123**',   //contraseña
+    'db' => 'DB_PRO_TSE'       //Nombre de tu base de datos
+];
+
+?>
