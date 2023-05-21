dfd9326dedfeca4754310a94bdddca6d651a66ae
Jose Linux <jperezp24@miumg.edu.gt>
2023-05-21 12:05:14 -0600

    Funcion partidos politicos

---
+++ partidopolitico.php
@@ -0,0 +1,25 @@
+<?php
+
+header("Content-Type: application/json");
+include "config.php";
+include "utils.php";
+
+
+$dbConn =  connect($db);
+
+/*
+  listar todos los posts o solo uno
+ */
+
+      //Mostrar lista de post
+      $sql = $dbConn->prepare("SELECT * FROM TB_PARTIDO_POLITICO");
+      $sql->execute();
+      $sql->setFetchMode(PDO::FETCH_ASSOC);
+      header("HTTP/1.1 200 OK");
+      echo json_encode( $sql->fetchAll()  );
+      exit();
+
+//En caso de que ninguna de las opciones anteriores se haya ejecutado
+header("HTTP/1.1 400 Bad Request");
+
+?>
